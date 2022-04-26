ec43aee9db4836ea32509557b74d19570166b396
alt <blaksteam@gmail.com>
2022-04-26 09:29:31 +0500

    update kt6

--- kt6/main.cpp
+++ kt6/main.cpp
@@ -1,13 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <string>
+
+using std::string;
 
 //var 15
 
-void solve(char *inpString){
-    char buffer[256];
+void solve(string inpString){
+    string buffer;
 
-    for (int i=0; i<strlen(inpString); i++){
-        printf("%d\n", i);
+    int spaceCount = 0;
+    for (int i=0; inpString.size()-3; i++){
+        std::cout << inpString.substr(i, 1);
+        if (inpString.substr(i, 1)==" ") spaceCount++;
+    }
+
+    std::cout << spaceCount << "\n";
+
+    size_t pos = inpString.find(" ");
+    while (pos != std::string::npos){
+        std::cout << inpString.substr(0, pos);
     }
 
     printf("\n\n");
@@ -19,11 +31,11 @@ int main() {
     int extcode = 0;
     short resp;
 
-    const char deff_string[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-    char proc_string[256] = "";
+    string deff_string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    string proc_string = "";
 
     while(extcode==0) {
-        printf("Определите способ ввода текста:\n1-Ввод из файла\n2-Ручной ввод\n3-Предустановленная строка:\n\n0-Выход\n>");
+        printf("Определите способ ввода текста:\n1-Ввод из файла\n2-Ручной ввод\n3-Предустановленная строка\n\n0-Выход\n>");
         scanf("%d", &resp);
 
         switch (resp) {
@@ -38,7 +50,7 @@ int main() {
                 break;
 
             case 3:
-                strcpy(proc_string, deff_string);
+                proc_string = deff_string;
                 break;
 
             case 0:
